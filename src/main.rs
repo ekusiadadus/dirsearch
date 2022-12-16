5a921ee36f2e0eb03ac8e22c134f8c889f3d1ef4
ekusiadadus <ekusiadadus@gmail.com>
2022-12-17 05:01:34 +0900

    feat: :fire: get directory and top 10 big files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::error::Error;
+
 use clap::Parser;
+use file_size::fit_4;
 use walkdir::WalkDir;
 
 /// Simple program to greet a person
@@ -14,9 +17,51 @@ struct Args {
     count: u8,
 }
 
+const DIR: &str = "./";
+const NUM: usize = 10;
+
+struct Entry {
+    path: String,
+    size: u64,
+}
+
 fn main() {
-    for entry in WalkDir::new(".") {
-        let entry = entry.unwrap();
-        println!("{}", entry.path().display());
+    match get_dir_size(DIR) {
+        Ok(_) => (),
+        Err(e) => println!("Error: {}", e),
     }
+}
+
+fn get_dir_size(dir: &str) -> Result<(), Box<dyn Error>> {
+    let mut size: u64 = 0;
+    let mut count: u64 = 0;
+    let mut tops: Vec<Entry> = Vec::with_capacity(NUM + 1);
+    let mut min_tops: u64 = 0;
+
+    for entry in WalkDir::new(dir).into_iter().filter_map(|e| e.ok()) {
+        let path = entry.path();
+        if path.is_file() {
+            let t = path.metadata().unwrap().len();
+            if t > min_tops {
+                tops.push(Entry {
+                    path: path.to_str().unwrap().to_string(),
+                    size: t,
+                });
+                tops.sort_by(|a, b| b.size.cmp(&a.size));
+                tops.truncate(NUM);
+                min_tops = tops.last().unwrap().size;
+            }
+            size += path.metadata().unwrap().len();
+            count += 1;
+        }
+    }
+
+    println!("{} files, {} bytes", count, fit_4(size));
+    println!("{} largest files:", NUM);
+    println!("{} | {}", "Size", "Path");
+    for t in tops {
+        println!("{} | {}", fit_4(t.size), t.path);
+    }
+
+    Ok(())
 }
